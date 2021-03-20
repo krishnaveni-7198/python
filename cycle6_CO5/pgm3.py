ae73ff8c65b666eee13e921c38a0f7320df2c688
krishnaveni-7198 <74653716+krishnaveni-7198@users.noreply.github.com>
2021-03-20 10:15:52 +0530

    Update pgm3.py

--- cycle6_CO5/pgm3.py
+++ cycle6_CO5/pgm3.py
@@ -1,5 +1,5 @@
 # Write a Python program to read each row from a given csv file and print a list of strings.
 
-f = open("student_data.csv","r")
+f = open("csvfile.csv","r")
 list = list(f.readlines())
 print(list)
